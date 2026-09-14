8b3890de3773e1a3e352b8f465eaa2a9489500bc
Hansel Bautista <hansbauhba@gmail.com>
2026-09-14 19:09:13 -0400

    feat: realizacion de la configuración de TypeOrmModule y conexión a PostgreSQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Cargar variables de entorno del .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // 2. Conexión a PostgreSQL vía TypeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true, // Sincroniza automáticamente los modelos con las tablas de PostgreSQL
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
